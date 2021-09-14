6f9fabe6141be246f209ffe53e2afd649546c7eb
Luukas <78479063+luukasmepham@users.noreply.github.com>
2021-09-14 12:15:37 +0300

    Basic structure initialized

---
+++ excercise_1/static_ui/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Helsingin Sanomat
+        </p>
+        <p>
+          Etusivu
+        </p>
+        <p>
+          Uutiset
+        </p>
+        <p>
+          Lehdet
+        </p>
+        <p>
+          Asiakaspalvelu
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

b2e2a55155966388277c13029c26016b097bad69
Luukas <78479063+luukasmepham@users.noreply.github.com>
2021-09-29 13:43:43 +0300

    Initiliazied excercise3

---
+++ excercise3/amazonsearch/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="search">
+        <p> Search </p>
+        <input type="search"></input>
+      </header>
+    </div>
+  );
+}
+
+export default App;

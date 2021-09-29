e39410e8bb99b5392e3605c72f4831be6bea3b56
Luukas <78479063+luukasmepham@users.noreply.github.com>
2021-09-29 14:31:58 +0300

    basic ui made

--- excercise3/amazonsearch/src/App.js
+++ excercise3/amazonsearch/src/App.js
@@ -4,10 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="search">
+      <header className="searchBox">
         <p> Search </p>
-        <input type="search"></input>
+        <input type="search" className="textField"></input>
       </header>
+      <div className='productTiles'>
+        <div className='temp'> <img src='https://offautan-uc1.azureedge.net/-/media/images/off/ph/products-en/products-landing/landing/off_kids_product_collections_large_2x.jpg?la=en-ph'/> </div>
+        <div className='temp'> <img src='https://offautan-uc1.azureedge.net/-/media/images/off/ph/products-en/products-landing/landing/off_kids_product_collections_large_2x.jpg?la=en-ph'/> </div>
+        <div className='temp'> <img src='https://offautan-uc1.azureedge.net/-/media/images/off/ph/products-en/products-landing/landing/off_kids_product_collections_large_2x.jpg?la=en-ph'/> </div>
+        <div className='temp'> <img src='https://offautan-uc1.azureedge.net/-/media/images/off/ph/products-en/products-landing/landing/off_kids_product_collections_large_2x.jpg?la=en-ph'/> </div>
+        <div className='temp'> <img src='https://offautan-uc1.azureedge.net/-/media/images/off/ph/products-en/products-landing/landing/off_kids_product_collections_large_2x.jpg?la=en-ph'/> </div>
+        <div className='temp'> <img src='https://offautan-uc1.azureedge.net/-/media/images/off/ph/products-en/products-landing/landing/off_kids_product_collections_large_2x.jpg?la=en-ph'/> </div>
+        <div className='temp'> <img src='https://offautan-uc1.azureedge.net/-/media/images/off/ph/products-en/products-landing/landing/off_kids_product_collections_large_2x.jpg?la=en-ph'/> </div>
+        <div className='temp'> <img src='https://offautan-uc1.azureedge.net/-/media/images/off/ph/products-en/products-landing/landing/off_kids_product_collections_large_2x.jpg?la=en-ph'/> </div>
+        <div className='temp'> <img src='https://offautan-uc1.azureedge.net/-/media/images/off/ph/products-en/products-landing/landing/off_kids_product_collections_large_2x.jpg?la=en-ph'/> </div>
+      </div>
     </div>
   );
 }
